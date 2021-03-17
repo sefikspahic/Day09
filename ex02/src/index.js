fed503b690ee5e68550d7a91c79ee6d5a66248bd
sefikspahice <spahicsefik@gmail.com>
2021-03-17 10:02:40 +0100

    Day09

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+function myFunction(myParameter) {
+  if (myParameter) {
+    return "The Parameter is true!";
+  }
+  return "The Parameter is false!";
+}
+
+console.log(myFunction(true));
+console.log(myFunction(false));
+// Only change code above this line
+
+module.exports = myFunction;
